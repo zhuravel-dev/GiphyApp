c6bf75d9375f95dbb6dc99b1c7a728f84a322036
Yuliya Zhuravel <zhuravel.dev@gmail.com>
2021-06-01 16:21:53 +0300

    Add unique key

--- app/src/main/java/com/example/giphyapp/MainActivity.kt
+++ app/src/main/java/com/example/giphyapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.giphyapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.giphy.sdk.ui.Giphy
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Giphy.configure(
+            this,
+            "5PqUpVUIFD2OwKoADDXgL4rTC4i7lgJh",
+            true,
+        )
+    
+
     }
 }
