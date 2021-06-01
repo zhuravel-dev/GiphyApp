3ba44d20e6f0a514f4b954800c27cbe88339d363
Yuliya Zhuravel <zhuravel.dev@gmail.com>
2021-06-01 19:14:43 +0300

    Add ProgressBar

--- app/src/main/java/com/example/giphyapp/MainActivity.kt
+++ app/src/main/java/com/example/giphyapp/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.giphyapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ProgressBar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.giphy.sdk.ui.Giphy
 
 class MainActivity : AppCompatActivity() {
+
+
+    private var progressBar: ProgressBar? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
