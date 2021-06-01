7be74fbabace32e661d2e794d49e2d3416ee6a3d
Yuliya Zhuravel <zhuravel.dev@gmail.com>
2021-06-01 17:35:51 +0300

    Create interface GetContent

---
+++ app/src/main/java/com/example/giphyapp/GetContent.kt
@@ -0,0 +1,14 @@
+package com.example.giphyapp
+
+import com.example.giphyapp.models.Json4Kotlin_Base
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface GetContent {
+    @GET("v1/gifs/search")
+    fun getContent(
+        @Query("limit") limit: Int = 25,
+        @Query("api_key") api_key: String = "5PqUpVUIFD2OwKoADDXgL4rTC4i7lgJh"
+    ): Call<Json4Kotlin_Base>
+}
